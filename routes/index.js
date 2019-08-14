6445794a3d7dc125dbfe26060602191a71509832
Shiyu Wang <shiyu3169@gmail.com>
2019-08-14 13:49:23 -0400

    redirect

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,19 @@ const Url = require('../models/Url');
 
 // @route GET /:code
 // @desc  Redirect to long/original URL
+router.get('/:code', async (req, res) => {
+  try {
+    const url = await Url.findOne({ urlCode: req.params.code });
+
+    if (url) {
+      return res.redirect(url.longUrl);
+    } else {
+      return res.status(404).json('No url found');
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).json('Server error');
+  }
+});
 
 module.exports = router;
