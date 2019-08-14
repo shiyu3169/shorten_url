124ff475570bd5174e3231488b6ed13caf11b43b
Shiyu Wang <shiyu3169@gmail.com>
2019-08-14 13:24:34 -0400

    parse long url into db

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const connectDB = require('./config/db');
+
 const app = express();
 
 // Connect to database
 connectDB();
 
-app.use(express.json({ extended: false }));
+app.use(express.json());
+
+// Define Routes
+app.use('/', require('./routes/index'));
+app.use('/api/url', require('./routes/url'));
 
 const PORT = 5000;
 
---
+++ routes/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+const Url = require('../models/Url');
+
+// @route GET /:code
+// @desc  Redirect to long/original URL
+
+module.exports = router;
